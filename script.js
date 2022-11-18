0b06c90f3e80225562115468b7fadaeb3689d924
victor <vito.daleste@gmail.com>
2022-11-18 20:56:55 -0300

    feat: script JS created

---
+++ script.js
@@ -0,0 +1,35 @@
+function handleClickPersa1(){
+    document.querySelector('#gato1').src = 'https://www.thesprucepets.com/thmb/5mrNYjDpNn0GnXD_crwBDGIijcs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/persian-cats-gallery-4121944-hero-f5c237b8c6404655afb1e1bbae219ba5.jpg';
+}
+
+function handleClickPersa2(){
+    document.querySelector('#gato1').src = 'https://d3544la1u8djza.cloudfront.net/APHI/Blog/2016/10_October/persians/Persian+Cat+Facts+History+Personality+and+Care+_+ASPCA+Pet+Health+Insurance+_+white+Persian+cat+resting+on+a+brown+sofa-min.jpg';
+}
+
+function handleClickPersa3(){
+    document.querySelector('#gato1').src = 'https://s1.1zoom.me/big0/915/Cats_Persian_cat_498254.jpg';
+}
+
+function handleClickNorueg1(){
+    document.querySelector('#gato2').src = 'https://rawznaturalpetfood.com/wp-content/uploads/Norwegian-Forest-Cats.jpeg';
+}
+
+function handleClickNorueg2(){
+    document.querySelector('#gato2').src = 'https://www.bubblypet.com/wp-content/uploads/2022/07/Young-black-Norwegian-Forest-cat-standing-in-a-garden.jpg';
+}
+
+function handleClickNorueg3(){
+    document.querySelector('#gato2').src = 'https://gatilspiritland.files.wordpress.com/2015/05/f6810bc5c3f8d2230a824404636f2409.jpg';
+}
+
+function handleClickRagdoll1(){
+    document.querySelector('#gato3').src = 'https://cdn.los-animales.org/fotos/3728117_1016936-lindos-filhotes-de-ragdoll.jpg';
+}
+
+function handleClickRagdoll2(){
+    document.querySelector('#gato3').src = 'https://ragdollcatsworld.com/wp-content/uploads/2021/03/joshua-mcarthur-E0rGtTlpvR4-unsplash-scaled.jpg';
+}
+
+function handleClickRagdoll3(){
+    document.querySelector('#gato3').src = 'https://cdn.vidaativa.pt/uploads/2019/11/87034-ragdoll_1528128230.jpg';
+}
